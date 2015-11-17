d982e6ec54cd05bd2fb3270edf6bfd35b850b58a
Randolph Burt <randolph@mbp.lan>
2015-11-17 22:06:47 +0000

    Fix constructor default values.

--- src/app/LindenmayerSystemDefinition.ts
+++ src/app/LindenmayerSystemDefinition.ts
@@ -18,7 +18,7 @@ export class LindenmayerSystemDefinition {
 export class LindenmayerSystemRule {
     input: string;
     output: string;
-    constructor(_input:string, _output:string) {
+    constructor(_input?:string, _output?:string) {
         this.input = _input;
         this.output = _output;
     }
